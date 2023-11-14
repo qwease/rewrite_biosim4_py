d5357e83d0c94cbb5a46d3f26480354397ffa636
Xuanxiang Zhao <3269964641@qq.com>
2023-11-14 04:14:43 +0100

    add "access params and modify randomUint()

--- simulator.py
+++ simulator.py
@@ -6,12 +6,18 @@ from path import Path
 if str(Path(__file__).parent) not in path:
     path.append(str(Path(__file__).parent)) 
 # print(path)
-from params import ParamManager
+from params import params,paramManager
 from grid import Grid
-from utils.RNG import randomUint,RandomUintGenerator
+from utils.RNG import getRandomGenerator
 
-grid= Grid()
-paramManager = ParamManager()
-params = paramManager.getParamRef()
-randomUint.instance=RandomUintGenerator()
-randomUint.instance.initialize(params=params) # now you can treat randomUint.instance as an object
+grid = Grid()
+randomUint=getRandomGenerator(p=params)
+
+# params = params
+
+# randomUint.instance=RandomUintGenerator()
+# randomUint.instance.initialize(params=params) # now you can treat randomUint.instance as an object
+
+
+# Check the following link to see ideas of how to implement main while loop
+# https://poe.com/s/VcbEhblIMAROzkEo1Xq9 
